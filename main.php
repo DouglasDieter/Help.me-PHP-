f340fdc741cc7b1539f97066736874bb6d425435
DouglasDieter <160547367+DouglasDieter@users.noreply.github.com>
2024-04-01 09:36:09 -0300

    Update main.php

--- main.php
+++ main.php
@@ -3,17 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style-main.css">
+    <link rel="stylesheet" href="stylemain.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Help.me</title>
 </head>
 <body>
-    <div class="wrap">
-        <div class="navbar">
-            
-        </div>
-        <div class="teste-um">
-            sasq
-        </div>
+  <nav class="navbar navbar-expand-lg barra">
+    <div class="container-fluid">
+      <a class="navbar titulo" href="#">Help.me</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="perfil.html">Acessar perfil</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button type="submit" class="btn btn-dark">Enviar</button>
+        </form>
+      </div>
     </div>
+  </nav>
+<a href="jogos.html" class="cartao-link">
+  <div class="card mb-3 head-cartao">
+    <div class="row g-0">
+      <div class="col-md-2 titulo-card">
+        <div class=><h5>Jogos</h5></div>
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <p class="card-text">Fórum dedicado à área de jogos (guias, dicas, discussões e mais).</p>
+          <p class="card-text"><small class="text-body-secondary">Criado por Usuário em Data</small></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</a>
+  <div class="add-forum">
+    <button type="button" class="botao-forum"> + Adicionar Fórum</button>
+  </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
 </body>
 </html>
